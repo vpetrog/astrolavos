03b059a15b642a38b7fb185b9dbdb40a31e9c730
Evangelos Petrongonas <vpetrog@ieee.org>
2025-06-13 10:42:42 +0200

    utils: intitial commit
    This library is a utility library for methods that will be used
    throughout the project. Currently it contains only a wrapper for the
    `vTaskDelay`.
    
    Signed-off-by: Evangelos Petrongonas <vpetrog@ieee.org>

---
+++ lib/utils/utils.hpp
@@ -0,0 +1,17 @@
+/**
+ * @file utils.hpp
+ * @author Evangelos Petrongonas (vpetrog@ieee.org)
+ * @brief Various common utilities that are used throughout the project
+ * @version 0.1
+ * @date 2025-06-13
+ *
+ * @copyright Copyright (c) 2025
+ *
+ */
+#include "freertos/FreeRTOS.h"
+
+namespace utils
+{
+/* -----------------------------  Helper macros ----------------------------- */
+static inline void delay_ms(uint32_t ms) { vTaskDelay(pdMS_TO_TICKS(ms)); }
+}
