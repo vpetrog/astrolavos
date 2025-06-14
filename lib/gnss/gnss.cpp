54cbea282b953575e1dd4dc7b441820c88ac1462
Evangelos Petrongonas <vpetrog@ieee.org>
2025-06-14 11:23:27 +0200

    gnss: Power down and sleep
    Total USB power consumptio can reach up to 80mAh. We are not driving at
    100kmh speed, therefore we can afford a lower refresh rate. We will be
    putting the device to light sleep for 30seconds, to preserve power.
    Meanwhile we will keep the display turned on.
    
    Signed-off-by: Evangelos Petrongonas <vpetrog@ieee.org>

--- lib/gnss/gnss.cpp
+++ lib/gnss/gnss.cpp
@@ -12,6 +12,7 @@
 #include "TinyGPS++.hpp"
 #include "driver/uart.h"
 #include "esp_log.h"
+#include "esp_sleep.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include <HT_st7735.hpp>
@@ -24,6 +25,19 @@ static const char* TAG = "GNSS";
 TinyGPSPlus gps;
 
 const size_t BUF_SIZE = 1024;
+constexpr size_t GNSS_TASK_SLEEP_TIMER = 30 * 1000000ULL;
+
+void gnss_power_up()
+{
+    gpio_set_level(heltec::PIN_GNSS_RST, 1);
+    utils::delay_ms(15);
+}
+
+void gnss_power_down()
+{
+    gpio_set_level(heltec::PIN_GNSS_RST, 0);
+    utils::delay_ms(15);
+}
 
 void gnss_task(void* args)
 {
@@ -40,10 +54,10 @@ void gnss_task(void* args)
     uart_driver_install(UART_NUM_1, BUF_SIZE * 2, 0, 8, nullptr, 0);
 
     uint8_t data[BUF_SIZE];
-
     while (true)
     {
-
+        gnss_power_up();
+        utils::delay_ms(3000);
         int len =
             uart_read_bytes(UART_NUM_1, data, BUF_SIZE, pdMS_TO_TICKS(100));
 
@@ -52,24 +66,27 @@ void gnss_task(void* args)
 
         if (gps.location.isUpdated())
         {
-            char buffer[180*3];
-            snprintf(buffer, sizeof(buffer), "No Satellites: %lu\nLat: %.5f\nLon: %.5f",
-                    gps.satellites.value(), gps.location.lat(), gps.location.lng());
+            char buffer[180 * 3];
+            snprintf(buffer, sizeof(buffer),
+                     "No Satellites: %lu\nLat: %.5f\nLon: %.5f",
+                     gps.satellites.value(), gps.location.lat(),
+                     gps.location.lng());
 
             ESP_LOGI(TAG, "%s", buffer);
 
-            display->fill_rectangle(0, 0, 180, 28,
-                                    ST7735_BLACK);
+            display->fill_rectangle(0, 0, 180, 28, ST7735_BLACK);
             display->write_str(0, 0, buffer, Font_7x10, ST7735_WHITE,
                                ST7735_BLACK);
+            gnss_power_down();
+            display->hold_pins();
+            esp_sleep_enable_timer_wakeup(GNSS_TASK_SLEEP_TIMER);
+            esp_light_sleep_start();
         }
         else
         {
-            display->fill_rectangle(0, 0, 180, 28,
-                                    ST7735_BLACK);
+            display->fill_rectangle(0, 0, 180, 28, ST7735_BLACK);
             display->write_str(0, 0, "Waiting for GNSS Data", Font_7x10,
-                               ST7735_WHITE,
-                               ST7735_BLACK);
+                               ST7735_WHITE, ST7735_BLACK);
             ESP_LOGI(TAG, "Waiting for GPS data... %d bytes", len);
         }
 
