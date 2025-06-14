08a60d3cc5d0aefa59f10f2479a6b1dc7fbf2b79
Evangelos Petrongonas <vpetrog@ieee.org>
2025-06-14 04:33:41 +0200

    gnss: Add gnss support and example
    Create a new gnss task that prints the current location on the screen.
    Important the app_main task, must not be terminated otherwise the
    display goes out of scope. In addition, we can't use an infinite busy
    waiting period due to a) power and b) it causes the watchdog timer to
    fire and pollute our logs (and probably messes up with the scheduler)
    
    Signed-off-by: Evangelos Petrongonas <vpetrog@ieee.org>

--- src/main.cpp
+++ src/main.cpp
@@ -15,79 +15,18 @@
 #include <pins.hpp>
 #include <utils.hpp>
 #include <HT_st7735.hpp>
+#include <gnss.hpp>
 
 static const char* TAG = "main";
 
-extern "C" void app_main(void)
+extern "C" void app_main()
 {
-    HT_st7735 st7735;
-
-    utils::delay_ms(500);
-    st7735.init();
-    ESP_LOGI(TAG, "TFT Initialised");
-
+    utils::delay_ms(1000);
+    HT_st7735 display;
+    display.init();
+    display.fill_screen(ST7735_BLACK);
+    xTaskCreate(gnss_task, "gnss_task", 4096, &display, 5, NULL);
     while (true)
-    {
-        st7735.fill_screen(ST7735_BLACK);
-
-        for (int x = 0; x < ST7735_WIDTH; x++)
-        {
-            st7735.draw_pixel(x, 0, ST7735_RED);
-            st7735.draw_pixel(x, ST7735_HEIGHT - 1, ST7735_RED);
-        }
-
-        for (int y = 0; y < ST7735_HEIGHT; y++)
-        {
-            st7735.draw_pixel(0, y, ST7735_RED);
-            st7735.draw_pixel(ST7735_WIDTH - 1, y, ST7735_RED);
-        }
-
-        vTaskDelay(pdMS_TO_TICKS(3000));
-
-        /* Demo Fonts */
-        st7735.fill_screen(ST7735_BLACK);
-        st7735.write_str(0, 0,
-                         "Font_7x10, red on black, lorem ipsum dolor sit amet",
-                         Font_7x10, ST7735_RED, ST7735_BLACK);
-        st7735.write_str(0, 3 * 10, "Font_11x18, green, lorem ipsum",
-                         Font_11x18, ST7735_GREEN, ST7735_BLACK);
-        st7735.write_str(0, 3 * 10 + 3 * 18, "Font_16x26", Font_16x26,
-                         ST7735_BLUE, ST7735_BLACK);
-        utils::delay_ms(2000);
-
-        /* Demo Colours */
-        st7735.fill_screen(ST7735_BLACK);
-        st7735.write_str(0, 0, "BLACK", Font_11x18, ST7735_WHITE, ST7735_BLACK);
-        utils::delay_ms(500);
-
-        st7735.fill_screen(ST7735_BLUE);
-        st7735.write_str(0, 0, "BLUE", Font_11x18, ST7735_BLACK, ST7735_BLUE);
-        utils::delay_ms(500);
-
-        st7735.fill_screen(ST7735_RED);
-        st7735.write_str(0, 0, "RED", Font_11x18, ST7735_BLACK, ST7735_RED);
-        utils::delay_ms(500);
-
-        st7735.fill_screen(ST7735_GREEN);
-        st7735.write_str(0, 0, "GREEN", Font_11x18, ST7735_BLACK, ST7735_GREEN);
-        utils::delay_ms(500);
-
-        st7735.fill_screen(ST7735_CYAN);
-        st7735.write_str(0, 0, "CYAN", Font_11x18, ST7735_BLACK, ST7735_CYAN);
-        utils::delay_ms(500);
-
-        st7735.fill_screen(ST7735_MAGENTA);
-        st7735.write_str(0, 0, "MAGENTA", Font_11x18, ST7735_BLACK,
-                         ST7735_MAGENTA);
-        utils::delay_ms(500);
-
-        st7735.fill_screen(ST7735_YELLOW);
-        st7735.write_str(0, 0, "YELLOW", Font_11x18, ST7735_BLACK,
-                         ST7735_YELLOW);
-        utils::delay_ms(500);
-
-        st7735.fill_screen(ST7735_WHITE);
-        st7735.write_str(0, 0, "WHITE", Font_11x18, ST7735_BLACK, ST7735_WHITE);
-        utils::delay_ms(500);
-    }
+        utils::delay_ms(5000); // keep the main task alive
+    
 }
