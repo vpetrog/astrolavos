vpetrog/astrolavos

commit a705c3273f661317bfbe0157a6f30aa019b75409

main: bug: fix nullptr dereference on blinking
If we are not running the astrolavos_app, the blinking task will try to
call a method from the display struct that does not exist. Fix it by
passing a nullptr and if so use a predefined sleep duration.

Fixes: d2341cd39456 ("Astrolavos: add a sleep_duration struct")
Signed-off-by: Evangelos Petrongonas <vpetrog@ieee.org>

src/main.cpp
@@ -28,6 +28,12 @@ void blinking_task(void* args)
 {
     astrolavos::Astrolavos* astrolavos_app =
         static_cast<astrolavos::Astrolavos*>(args);
+    size_t sleep_duration;
+    if (astrolavos_app)
+        sleep_duration =
+            astrolavos_app->getSleepDuration()->blinking;
+    else
+        sleep_duration = 1500; // Default blinking duration if no app provided
     gpio_set_direction(heltec::PIN_LED_WRITE_CTRL, GPIO_MODE_OUTPUT);
     ESP_LOGI(TAG, "Blinking Task started");
     while (true)
@@ -37,12 +43,12 @@ void blinking_task(void* args)
         gpio_set_level(heltec::PIN_LED_WRITE_CTRL, 1);
         gpio_hold_en(heltec::PIN_LED_WRITE_CTRL);
 
-        utils::delay_ms(astrolavos_app->getSleepDuration()->blinking);
+        utils::delay_ms(sleep_duration);
 
         gpio_hold_dis(heltec::PIN_LED_WRITE_CTRL);
         gpio_set_level(heltec::PIN_LED_WRITE_CTRL, 0);
         gpio_hold_en(heltec::PIN_LED_WRITE_CTRL);
-        utils::delay_ms(astrolavos_app->getSleepDuration()->blinking);
+        utils::delay_ms(sleep_duration);
     }
 }
 
@@ -65,7 +71,7 @@ extern "C" void app_main()
 
     xTaskCreate(gnss_task, "gnss_task", 4096, &display, 5, NULL);
     xTaskCreate(battery_task, "battery_task", 4096, &display, 2, NULL);
-    xTaskCreate(blinking_task, "blinking_task", 4096, &display, 3, NULL);
+    xTaskCreate(blinking_task, "blinking_task", 4096, nullptr, 3, NULL);
     xTaskCreate(heading_task, "heading_task", 4096, &display, 4, NULL);
 
     vTaskSuspend(NULL);

commit 7dde6de28839f6ab187907177f3ffb03ee034e22

main: fix sleep duration in blinking task
Blinking is broken again. This time, because the sleep duration as
defined in the app struct is not reflected to the actual sleep duration.
Let's fix it hopefully once and for all.

Fixes: a705c3273f66 ("main: bug: fix nullptr dereference on blinking")
Signed-off-by: Evangelos Petrongonas <vpetrog@ieee.org>

src/main.cpp
@@ -31,14 +31,15 @@ void blinking_task(void* args)
         static_cast<astrolavos::Astrolavos*>(args);
     size_t sleep_duration;
     if (astrolavos_app)
-        sleep_duration =
-            astrolavos_app->getSleepDuration()->blinking;
+        sleep_duration = astrolavos_app->getSleepDuration()->blinking;
     else
         sleep_duration = 1500; // Default blinking duration if no app provided
     gpio_set_direction(heltec::PIN_LED_WRITE_CTRL, GPIO_MODE_OUTPUT);
     ESP_LOGI(TAG, "Blinking Task started");
     while (true)
     {
+        if (astrolavos_app)
+            sleep_duration = astrolavos_app->getSleepDuration()->blinking;
         ESP_LOGI(TAG, "Blinking");
         gpio_hold_dis(heltec::PIN_LED_WRITE_CTRL);
         gpio_set_level(heltec::PIN_LED_WRITE_CTRL, 1);