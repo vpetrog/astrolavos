vpetrog/astrolavos

commit 4fe8a677a2e4c3878be3378eee0cd58b3be5166d

Astrolavos: Introduce setup mode
The system needs to have a configuration mode. In this mode the user
will be able to setup different parameters. In order to enter this mode
the user will need to press the usr_switch button while the system is
booting. Once the boot process is finished, the setupMode method is
called, the functions associated with this mode are called and in the
end the system is rebooted.

Currently there are no setup functions. These will be added in a future
commit.

Signed-off-by: Evangelos Petrongonas <vpetrog@ieee.org>

lib/Astrolavos/Astrolavos.cpp
@@ -12,6 +12,7 @@
 #include "driver/gpio.h"
 #include "esp_log.h"
 #include "esp_pm.h"
+#include "esp_system.h"
 #include "esp_timer.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
@@ -378,6 +379,19 @@ void Astrolavos::updateIWantToMeet(bool i_want_to_meet)
     _i_want_to_meet = i_want_to_meet;
 }
 
+bool Astrolavos::isBooted() { return _is_booted; }
+
+bool Astrolavos::isSetupRequested() { return _setup_requested; }
+
+void Astrolavos::requestSetup() { _setup_requested = true; }
+
+void Astrolavos::setupMode()
+{
+    ESP_LOGI(TAG, "Entering Setup Mode");
+    utils::delay_ms(3000);
+    esp_restart();
+}
+
 void Astrolavos::refreshHealthBar()
 {
     /* The Display row is 160 pixels Wide, with at a Font size of 7x10
@@ -516,7 +530,12 @@ void usr_button_isr_handler(void* args)
     astrolavos::Astrolavos* astrolavos_app =
         static_cast<astrolavos::Astrolavos*>(args);
     if (astrolavos_app)
-        astrolavos_app->triggerIsolationMode();
+    {
+        if (astrolavos_app->isBooted())
+            astrolavos_app->triggerIsolationMode();
+        else
+            astrolavos_app->requestSetup();
+    }
 }
 
 void iwtm_isr_handler(void* args)
@@ -584,13 +603,14 @@ void Astrolavos::init(HT_st7735* display)
                         Font_11x18, _color, ST7735_BLACK);
     _display->hold_pins();
     initialisePairedDevices();
+    initSwitchInterrupt();
     utils::delay_ms(ASTROLAVOS_WELCOME_SLEEP);
     _display->unhold_pins();
     _display->fill_screen(ST7735_BLACK);
     _display->hold_pins();
-    initSwitchInterrupt();
     initIWTMInterrupt();
 
+    _is_booted = true;
     ESP_LOGI(TAG, "Astrolavos initialized with ID: %d, Name: %s", _id, _name);
 }
 } // namespace astrolavos
@@ -606,7 +626,12 @@ void astrolavos_task(void* args)
     HT_st7735* display = reinterpret_cast<HT_st7735*>(task_args->display);
     ESP_LOGI(TAG, "Astrolavos Task started");
     astrolavos_app->init(display);
-    esp_pm_lock_release(lock);
+    if (astrolavos_app->isSetupRequested())
+    {
+        ESP_LOGI(TAG, "Setup requested, entering setup mode");
+        astrolavos_app->setupMode();
+        esp_pm_lock_release(lock);
+    }
     while (true)
     {
         esp_pm_lock_acquire(lock);

lib/Astrolavos/Astrolavos.hpp
@@ -164,6 +164,33 @@ public:
      */
     bool getIWantToMeet();
 
+    /**
+     * @brief Check if Astrolavos is booted.
+     *
+     */
+    bool isBooted();
+
+    /**
+     * @brief Check if the user has indicated that they want to perform setup
+     *
+     * @return true
+     * @return false
+     */
+    bool isSetupRequested();
+
+    /**
+     * @brief Request to enter setup instead of the main application.
+     * 
+     */
+    void requestSetup();
+
+    /**
+     * @brief Astrolavos goes into setup mode. IN this mode the user can configure
+     * various settings and operational parameters.
+     * 
+     */
+    void setupMode();
+
 private:
     /**
      * @brief Calculate the heading to the device with the given ID.
@@ -245,7 +272,9 @@ private:
     bool _isolation_mode_triggered = false; /* Isolation mode flag */
     bool _isolation_mode = false;           /* Isolation mode */
     const sleep_duration_t* _sleep_duration = nullptr;
-    bool _i_want_to_meet = false; /* Indicates whether I want to meet */
+    bool _i_want_to_meet = false;  /* Indicates whether I want to meet */
+    bool _is_booted = false;       /* Indicates whether Astrolavos is booted */
+    bool _setup_requested = false; /* Indicates whether setup is requested */
 };
 
 typedef struct

commit 91eea41c2c5fac39af1d9eb9ed43f0f8c7a1c5f4

Astrolavos: fix device never going to sleep
With the introduction of setup mode, we have misplaced a pm_lock_release
and as a result the system could never go into sleep mode.

Fixes: 4fe8a677a2e4 ("Astrolavos: Introduce setup mode")
Signed-off-by: Evangelos Petrongonas <vpetrog@ieee.org>

lib/Astrolavos/Astrolavos.cpp
@@ -677,8 +677,8 @@ void astrolavos_task(void* args)
     {
         ESP_LOGI(TAG, "Setup requested, entering setup mode");
         astrolavos_app->setupMode();
-        esp_pm_lock_release(lock);
     }
+    esp_pm_lock_release(lock);
     while (true)
     {
         esp_pm_lock_acquire(lock);