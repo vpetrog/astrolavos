bc46ab0163ee468069570a121ad04376e9461773
Evangelos Petrongonas <vpetrog@ieee.org>
2025-06-21 16:01:31 +0200

    lora: add Astrolavos rx & tx tasks
    - TX: we implement a blocking transmit that fires periodically. We have
      also added a random delay on top to avoid any deadlocks and congestion
    issues with other devices.
    - RX: Implement a non-blocking receive. Unfortunately, the current
      implementation is trash since we are not directly waking up the task
    but instead poll for a flag to be true and then handle the received
    message in a loop. This is subpar but Fusion is closing in and we don't
    have enough time to debug everything. There is a possibility of dropping
    messages if two messages are received while the task is sleeping,
    therefore we should either decrease the sleep duration or fix it for
    good by waking up on the interrupt
    
    Issue: #21 #22
    Signed-off-by: Evangelos Petrongonas <vpetrog@ieee.org>

--- lib/lora/lora.cpp
+++ lib/lora/lora.cpp
@@ -143,5 +143,160 @@ void lora_tx_task(void* args)
         utils::delay_ms(1000);
     }
 }
-#endif
+#else
 
+void lora_tx_astrolavos_task(void* args)
+{
+    astrolavos::Astrolavos* astrolavos_app =
+        static_cast<astrolavos::Astrolavos*>(args);
+
+    esp_pm_lock_handle_t lock;
+    esp_pm_lock_create(ESP_PM_CPU_FREQ_MAX, 0, "lora_rx_lock", &lock);
+    esp_pm_lock_acquire(lock);
+    int err;
+    /* We should really do a better job at synchronising, but yeah it is good
+     * enough*/
+    utils::delay_ms(SX1262_BOOT_TIME_DELAY);
+
+    LoRa* lora = astrolavos_app->getLoRa();
+    ESP_LOGI(TX_TAG, "LoRa TX task started");
+    esp_pm_lock_release(lock);
+
+    while (true)
+    {
+        esp_pm_lock_acquire(lock);
+        ESP_LOGI(TX_TAG, "Transmitting");
+        SX1262* radio = lora->getRadio();
+        err = radio->standby();
+        if (err != RADIOLIB_ERR_NONE)
+        {
+            ESP_LOGE(TX_TAG, "Failed to put radio in standby: %d", err);
+            lora->putRadio();
+            esp_pm_lock_release(lock);
+            continue; // Skip to the next iteration
+        }
+
+        astrolavos::application_message_t msg =
+            astrolavos_app->constructMessage();
+
+        if (msg.id == astrolavos::ID_ASTROLAVOS_NOT_INITIALIZED)
+        {
+            ESP_LOGE(TX_TAG, "Astrolavos does not have valid coordinates "
+                             "cannot transmit message");
+            esp_pm_lock_release(lock);
+            utils::delay_ms(astrolavos_app->getSleepDuration()->lora_tx);
+            continue;
+        }
+        int16_t err =
+            radio->transmit(reinterpret_cast<const uint8_t*>(&msg),
+                            sizeof(astrolavos::application_message_t));
+        lora->putRadio();
+        if (err != RADIOLIB_ERR_NONE)
+        {
+            ESP_LOGE(TX_TAG, "Failed to Transmit message: %d", err);
+        }
+        esp_pm_lock_release(lock);
+
+        /*Add a random delay to avoid congestion*/
+        utils::delay_ms(astrolavos_app->getSleepDuration()->lora_tx +
+                        esp_random() % SX1262_MAX_RANDOM_TX_DELAY);
+    }
+}
+
+static void IRAM_ATTR onDio1() { receivedFlag = true; }
+
+void lora_rx_astrolavos_task(void* args)
+{
+    auto* astrolavos_app = static_cast<astrolavos::Astrolavos*>(args);
+    LoRa* lora = astrolavos_app->getLoRa();
+    int err;
+    // 1) PM lock & cold-boot delay (same as TX task)
+    esp_pm_lock_handle_t lock;
+    esp_pm_lock_create(ESP_PM_CPU_FREQ_MAX, 0, "lora_rx_lock", &lock);
+    esp_pm_lock_acquire(lock);
+    astrolavos::application_message_t received_message;
+
+    /* Ugly delay */
+    utils::delay_ms(SX1262_BOOT_TIME_DELAY);
+
+    /* Configure Interrupts */
+    SX1262* radio = lora->getRadio();
+    radio->setPacketReceivedAction(onDio1);
+
+    err = radio->startReceive();
+    if (err != RADIOLIB_ERR_NONE)
+    {
+        ESP_LOGE(RX_TAG, "Failed to start RX: %d", err);
+        lora->putRadio();
+        vTaskSuspend(nullptr);
+    }
+    lora->putRadio();
+    ESP_LOGI(RX_TAG, "LoRa RX task started (non-blocking)");
+
+    esp_pm_lock_release(lock);
+
+    while (true)
+    {
+        if (receivedFlag)
+        {
+            ESP_LOGI(RX_TAG, "Packet received, processing...");
+            receivedFlag = false; // Reset the flag
+
+            esp_pm_lock_acquire(lock);
+            SX1262* r = lora->getRadio();
+
+            /* Block Receiving more messages */
+            if (r->standby() != RADIOLIB_ERR_NONE)
+            {
+                ESP_LOGE(RX_TAG, "Failed to put radio in standby: %d", err);
+                lora->putRadio();
+                esp_pm_lock_release(lock);
+                continue; // Skip to the next iteration
+            }
+
+            int len = radio->getPacketLength();
+
+            if (len % sizeof(astrolavos::application_message_t) != 0)
+            {
+                if (len == 0)
+                    ESP_LOGW(RX_TAG, "Received packet length is 0, skipping");
+                else
+                    ESP_LOGE(RX_TAG,
+                             "Received packet length is not a multiple of "
+                             "application message size: %d",
+                             len);
+                lora->putRadio();
+                esp_pm_lock_release(lock);
+                continue; // Skip to the next iteration
+            }
+            int number_of_messages =
+                len / sizeof(astrolavos::application_message_t);
+            ESP_LOGI(RX_TAG, "Handling: %d number of messages",
+                     number_of_messages);
+
+            for (int i = 0; i < number_of_messages; i++)
+            {
+                /* Initially we hoped that we can read multiple packets from the
+                 * Queue. Apparently we can read only the last one. I am not
+                 * convinced that this can not be done in the future, lets keep
+                 * it like that for now, even if the loop is useless */
+                err = r->readData(reinterpret_cast<uint8_t*>(&received_message),
+                                  sizeof(astrolavos::application_message_t));
+                if (err != RADIOLIB_ERR_NONE)
+                {
+                    ESP_LOGE(RX_TAG, "Failed to read data: %d", err);
+                    lora->putRadio();
+                    esp_pm_lock_release(lock);
+                    continue; // Skip to the next iteration
+                }
+                astrolavos_app->handleReceivedMessage(received_message);
+            }
+
+            radio->startReceive();
+            lora->putRadio();
+            esp_pm_lock_release(lock);
+        }
+        utils::delay_ms(astrolavos_app->getSleepDuration()->lora_rx);
+    }
+}
+#endif
