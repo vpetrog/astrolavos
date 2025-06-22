88045703edca2a05ea6a838ca8d5f479bd1c3f5e
Evangelos Petrongonas <vpetrog@ieee.org>
2025-06-22 18:49:26 +0200

    main: Remove blinking task
    We are mature enough to remove the blinking task. But we won't remove it
    just yet from the code. If all fails we can always start blinking again
    
    Signed-off-by: Evangelos Petrongonas <vpetrog@ieee.org>

--- src/main.cpp
+++ src/main.cpp
@@ -26,6 +26,7 @@
 
 static const char* TAG = "main";
 
+#if 0
 void blinking_task(void* args)
 {
     astrolavos::Astrolavos* astrolavos_app =
@@ -54,43 +55,8 @@ void blinking_task(void* args)
         utils::delay_ms(sleep_duration);
     }
 }
+#endif
 
-#if 0
-extern "C" void app_main()
-{
-
-    esp_err_t err = nvs_flash_init();
-    if (err == ESP_ERR_NVS_NO_FREE_PAGES ||
-        err == ESP_ERR_NVS_NEW_VERSION_FOUND)
-    {
-        ESP_LOGI(TAG, "NVS Flash needs to be erased, re-initializing");
-        ESP_ERROR_CHECK(nvs_flash_erase());
-        err = nvs_flash_init();
-    }
-    ESP_ERROR_CHECK(err);
-    ESP_LOGI(TAG, "NVS Flash initialized");
-
-    HT_st7735 display;
-    display.init();
-    display.set_backlight(10);
-    display.fill_screen(ST7735_BLACK);
-
-    esp_pm_config_t pm_config = {
-        .max_freq_mhz = 240,
-        .min_freq_mhz = 10,
-        .light_sleep_enable = true,
-    };
-
-    ESP_ERROR_CHECK(esp_pm_configure(&pm_config));
-
-    xTaskCreate(gnss_task, "gnss_task", 4096, &display, 5, NULL);
-    xTaskCreate(battery_task, "battery_task", 4096, &display, 2, NULL);
-    xTaskCreate(blinking_task, "blinking_task", 4096, nullptr, 3, NULL);
-    xTaskCreate(heading_task, "heading_task", 4096, &display, 4, NULL);
-
-    vTaskSuspend(NULL);
-}
-#else
 extern "C" void app_main()
 {
     HT_st7735 display;
@@ -128,7 +94,6 @@ extern "C" void app_main()
                 NULL);
     xTaskCreate(battery_astrolavos_task, "battery_task", 4096, &astrolavos_app,
                 2, NULL);
-    xTaskCreate(blinking_task, "blinking_task", 4096, &astrolavos_app, 3, NULL);
     xTaskCreate(heading_astrolavos_task, "heading_task", 4096, &astrolavos_app,
                 4, NULL);
 #if defined(ASTROLAVOS_MOCKUP_LORA_RECEIVER)
@@ -142,4 +107,3 @@ extern "C" void app_main()
 #endif
     vTaskSuspend(NULL);
 }
-#endif
