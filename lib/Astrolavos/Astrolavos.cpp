vpetrog/astrolavos

commit d2341cd3945610a8e7361bfd474fed71e0784ff7

Astrolavos: add a sleep_duration struct
In preparation of the introduction of an isolation mode, which is going
to modify among other things the sleep duration. THis commit changes the
constant definitions and uses a struct in the astrolavos app to pass the
sleep duration around.

Signed-off-by: Evangelos Petrongonas <vpetrog@ieee.org>

lib/Astrolavos/Astrolavos.cpp
@@ -28,11 +28,17 @@ extern paired_device_auto_config_t
 extern paired_device_auto_config_t this_device;
 
 constexpr size_t ASTROLAVOS_WELCOME_SLEEP = 3 * 1000;
-constexpr size_t ASTROLAVOS_MAIN_LOOP_SLEEP = 2 * 1000;
 
 constexpr float EARTH_RADIUS_M = 6371000.0f; // Radius of the Earth in meters
 
 constexpr float ASTROLAVOS_MAXIMUM_ACCEPTABLE_DISTANCE = 30000; /* 30km */
+
+const sleep_duration_t normal_sleep_duration = {
+    .heading = 1000,          /* 1 second */
+    .main_app_refresh = 2000, /* 2 seconds */
+    .battery = 60000,         /* 1 minute */
+    .blinking = 1500          /* 3.5 seconds */
+};
 
 void Astrolavos::updateHealthBattery(uint8_t percentage)
 {
@@ -305,6 +311,11 @@ void Astrolavos::initialisePairedDevices()
     }
 }
 
+const sleep_duration_t* Astrolavos::getSleepDuration()
+{
+    return _sleep_duration;
+}
+
 AstrolavosPairedDevice* Astrolavos::getDevice(int id)
 {
     if (id < 0)
@@ -479,7 +490,6 @@ void astrolavos_task(void* args)
             astrolavos_app->refreshDevice(i);
         }
         esp_pm_lock_release(lock);
-        utils::delay_ms(
-            astrolavos::ASTROLAVOS_MAIN_LOOP_SLEEP);
+        utils::delay_ms(astrolavos_app->getSleepDuration()->main_app_refresh);
     }
 }

lib/Astrolavos/Astrolavos_types.hpp
@@ -11,8 +11,8 @@
 
 #pragma once
 
+#include <cstddef>
 #include <cstdint>
-
 namespace astrolavos
 {
 
@@ -90,6 +90,14 @@ typedef enum
     ASTROLAVOS_DIRECTION_FRONT_RIGHT,
     ASTROLAVOS_DIRECTION_UNKNOWN
 } direction_t;
+
+typedef struct
+{
+    std::size_t heading;
+    std::size_t main_app_refresh;
+    std::size_t battery;
+    std::size_t blinking;
+} sleep_duration_t;
 
 typedef struct
 {

lib/Astrolavos/Astrolavos.hpp
@@ -93,6 +93,13 @@ public:
     void refreshDevice(int id);
 
     /**
+     * @brief Get the Sleep Duration
+     *
+     * @return const sleep_duration_t*
+     */
+    const sleep_duration_t* getSleepDuration();
+
+    /**
      * @brief Get the current coordinates
      *
      * @return gnss_location_t
@@ -166,6 +173,7 @@ private:
     char _name[6];                           /* Name of the Astrolavos device */
     uint16_t _color = 0x0000;
     SemaphoreHandle_t _health_mutex = nullptr;
+    const sleep_duration_t* _sleep_duration = nullptr;
 };
 
 typedef struct

lib/BatteryMonitor/BatteryMonitor.cpp
@@ -71,8 +71,7 @@ BatteryMonitor::BatteryMonitor(adc_channel_t channel)
     _channel = channel;
 
     gpio_reset_pin(heltec::PIN_ADC_CTRL);
-    gpio_set_direction(heltec::PIN_ADC_CTRL,
-                       GPIO_MODE_OUTPUT); // Disable digital input/output
+    gpio_set_direction(heltec::PIN_ADC_CTRL, GPIO_MODE_OUTPUT);
 
     adc_oneshot_unit_init_cfg_t unit_cfg{
         .unit_id = ADC_UNIT_1,
@@ -82,10 +81,8 @@ BatteryMonitor::BatteryMonitor(adc_channel_t channel)
     ESP_ERROR_CHECK(adc_oneshot_new_unit(&unit_cfg, &_adc));
 
     /* configure the specific channel (GPIO 1  → ADC1_CH0) */
-    adc_oneshot_chan_cfg_t chan_cfg{
-        .atten = ADC_ATTEN_DB_12,
-        .bitwidth = ADC_BITWIDTH_12 // 12-bit on ESP32-S3
-    };
+    adc_oneshot_chan_cfg_t chan_cfg{.atten = ADC_ATTEN_DB_12,
+                                    .bitwidth = ADC_BITWIDTH_12};
     ESP_ERROR_CHECK(adc_oneshot_config_channel(_adc, _channel, &chan_cfg));
 }
 
@@ -179,6 +176,6 @@ void battery_astrolavos_task(void* args)
 
         ESP_LOGI(TAG, "Battery Voltage: %.2f V Raw=%d", voltage, raw);
         esp_pm_lock_release(lock);
-        utils::delay_ms(BATTERY_TASK_SLEEP);
+        utils::delay_ms(astrolavos_app->getSleepDuration()->battery);
     }
 }

lib/QMC5883L/QMC5883L.cpp
@@ -175,6 +175,7 @@ void heading_astrolavos_task(void* args)
                 astrolavos::magnetometer_health_t::MAGNETOMETER_ERROR);
         }
         esp_pm_lock_release(lock);
-        utils::delay_ms(HEADING_TASK_SLEEP); // Adjust delay as needed
+        utils::delay_ms(astrolavos_app->getSleepDuration()
+                            ->heading);
     }
 }

src/main.cpp
@@ -24,10 +24,10 @@
 
 static const char* TAG = "main";
 
-constexpr size_t BLINKING_TASK_SLEEP = 3500;
-
 void blinking_task(void* args)
 {
+    astrolavos::Astrolavos* astrolavos_app =
+        static_cast<astrolavos::Astrolavos*>(args);
     gpio_set_direction(heltec::PIN_LED_WRITE_CTRL, GPIO_MODE_OUTPUT);
     ESP_LOGI(TAG, "Blinking Task started");
     while (true)
@@ -37,12 +37,12 @@ void blinking_task(void* args)
         gpio_set_level(heltec::PIN_LED_WRITE_CTRL, 1);
         gpio_hold_en(heltec::PIN_LED_WRITE_CTRL);
 
-        utils::delay_ms(BLINKING_TASK_SLEEP);
+        utils::delay_ms(astrolavos_app->getSleepDuration()->blinking);
 
         gpio_hold_dis(heltec::PIN_LED_WRITE_CTRL);
         gpio_set_level(heltec::PIN_LED_WRITE_CTRL, 0);
         gpio_hold_en(heltec::PIN_LED_WRITE_CTRL);
-        utils::delay_ms(BLINKING_TASK_SLEEP);
+        utils::delay_ms(astrolavos_app->getSleepDuration()->blinking);
     }
 }
 
@@ -94,7 +94,7 @@ extern "C" void app_main()
                 NULL);
     xTaskCreate(battery_astrolavos_task, "battery_task", 4096, &astrolavos_app,
                 2, NULL);
-    xTaskCreate(blinking_task, "blinking_task", 4096, nullptr, 3, NULL);
+    xTaskCreate(blinking_task, "blinking_task", 4096, &astrolavos_app, 3, NULL);
     xTaskCreate(heading_astrolavos_task, "heading_task", 4096, &astrolavos_app,
                 4, NULL);
 #if defined(ASTROLAVOS_MOCKUP_LORA_RECEIVER)

commit 005a19923b8062b7ecf93aa3183e31a54e724f45

Astrolavos, gnss: control the sleep from astrolavos
We are already changing the sleep duration on the other tasks based on
the mode we are. Apparently, we forgot to do the same for gnss. This
commits does exactly that and while at it changes the sleep duration to
better reflect the operational needs.

Fixes: d2341cd39456 ("Astrolavos: add a sleep_duration struct")
Signed-off-by: Evangelos Petrongonas <vpetrog@ieee.org>

lib/Astrolavos/Astrolavos.cpp
@@ -43,7 +43,9 @@ const sleep_duration_t normal_sleep_duration = {
     .battery = 60000,         /* 1 minute */
     .blinking = 1500,         /* 1.5 seconds */
     .lora_rx = 500,           /* 1 second */
-    .lora_tx = 45000          /* 45 second */
+    .lora_tx = 45000,         /* 45 second */
+    .gnss = 20000, /* 20 seconds We need a balance between calculating our
+                     distance to other and saving powr scanning sleep */
 };
 
 const sleep_duration_t isolation_sleep = {
@@ -54,7 +56,9 @@ const sleep_duration_t isolation_sleep = {
     .battery = 300000,        /* 5 minutes */
     .blinking = 5000,         /* 5 seconds */
     .lora_rx = 15000,         /* 1 second */
-    .lora_tx = 45000          /* 45 second */
+    .lora_tx = 45000,         /* 45 second */
+    .gnss = 45000, /* 45 seconds We are not actively tring to find anyone else,
+                      so roughly sync it with the tx sleep */
 };
 
 void Astrolavos::updateHealthBattery(uint8_t percentage)

lib/Astrolavos/Astrolavos_types.hpp
@@ -107,6 +107,7 @@ typedef struct
     std::size_t blinking;
     std::size_t lora_rx;
     std::size_t lora_tx;
+    std::size_t gnss;
 } sleep_duration_t;
 
 typedef struct

lib/gnss/gnss.cpp
@@ -140,7 +140,7 @@ void gnss_astrolavos_task(void* args)
         if (gps.location.isUpdated())
         {
             ESP_LOGI(TAG,
-                     "GNSS Data Updated,nUM Satellites: %lu, "
+                     "GNSS Data Updated,Num Satellites: %lu, "
                      "Lat: %.5f, Lon: %.5f",
                      gps.satellites.value(), gps.location.lat(),
                      gps.location.lng());
@@ -152,7 +152,7 @@ void gnss_astrolavos_task(void* args)
                  static_cast<uint32_t>(esp_timer_get_time())});
             gnss_power_down();
             esp_pm_lock_release(lock);
-            utils::delay_ms(GNSS_TASK_LOCATED_SLEEP);
+            utils::delay_ms(astrolavos_app->getSleepDuration()->gnss);
         }
         else
         {